e457496e7af14e4a67824517551e88d2d3461c3b
Francesco Cesareo <cesareo.francesco@gmail.com>
2021-10-15 17:15:52 +0200

    fix jest configuration

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
+/**
+ * @jest-environment jsdom
+ */
+
 test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
