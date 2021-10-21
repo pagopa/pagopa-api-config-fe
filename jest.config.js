ccb3b1117985e5a935a7b60e8dc48ecda3206d0e
Francesco Cesareo <cesareo.francesco@gmail.com>
2021-10-21 18:17:29 +0200

    test CLI scan mode

--- jest.config.js
+++ jest.config.js
@@ -180,7 +180,6 @@ module.exports = {
   // This option allows the use of a custom results processor
   // testResultsProcessor: undefined,
   "testResultsProcessor": "jest-sonar-reporter"
-  // "testResultsProcessor": "jest-junit"
 
   // This option allows use of a custom test runner
   // testRunner: "jest-circus/runner",
