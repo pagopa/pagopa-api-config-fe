2bcd7f01e874c09d0072bbc7458ac5ef015e0738
Francesco Cesareo <cesareo.francesco@gmail.com>
2021-11-15 10:35:13 +0100

    [PCFG-89] env generation script (#18)
    * added azure pipeline yml file
    
    * added comment
    
    * fix build stage, removed local link
    
    * improved deploy pipeline
    
    * improved deploy pipeline
    
    * fix indentation
    
    * env generation script
    
    * fix azure subscription
    
    * removed condition
    
    * test
    
    * fix dist folder
    
    * fix storage account name
    
    * changed vm
    
    * fix bundle
    
    * set depends on
    
    * changed deploy task
    
    * fix destination
    
    * added cdn purge
    
    * env generation script
    
    * added comment in env config generation
    
    * fix test stage to work with generated env
    
    * added prebuild env config generation
    
    * fix yml
    
    * fix azure subscription param
    
    * changed variables name
    
    * env generation script
    
    * added comment in env config generation
    
    * modified source command
    
    * fix test stage to work with generated env
    
    * added prebuild env config generation
    
    * added envconfig script

---
+++ env.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Recreate config file and assignment
+echo "// Generated automatically, please don't write here" > ./src/env-config.js
+echo "" >> ./src/env-config.js
+echo "window._env_ = {" >> ./src/env-config.js
+
+# Loop on environment variables prefixed with
+# apiconfig_ and add them to env-config.js
+for apiconfig_var in $(env | grep -i apiconfig_); do
+    varname=$(printf '%s\n' "$apiconfig_var" | sed -e 's/=.*//')
+    varvalue=$(printf '%s\n' "$apiconfig_var" | sed -e 's/^[^=]*=//')
+
+    echo "  $varname: \"$varvalue\"," >> ./src/env-config.js
+done
+
+echo "};" >> ./src/env-config.js
