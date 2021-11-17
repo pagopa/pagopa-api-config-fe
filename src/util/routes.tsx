396922e658b5a00e332e585309f904d49cdae307
Francesco Cesareo <cesareo.francesco@gmail.com>
2021-11-17 13:38:59 +0100

    fix routes

--- src/util/routes.tsx
+++ src/util/routes.tsx
@@ -25,7 +25,7 @@ export default class Routes extends React.Component {
                                         const edit: boolean = new URLSearchParams(props.location.search).get("edit") !== null;
                                         return edit ? <EditCreditorInstitution {...props} /> : <CreditorInstitution {...props} />;
                                     }}/>
-                                    <Route path="/brokers" component={Broker}/>
+                                    <Route path="/brokers" component={BrokersPage}/>
                                     <Route path="/brokers/:code" component={BrokerPage} />
                                     <Route component={NotFound}/>
                                 </Switch>
