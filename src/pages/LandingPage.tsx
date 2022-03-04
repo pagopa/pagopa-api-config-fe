55b98f05486212ee4a8d1c7ee578dfe153dc369c
Francesco Cesareo <cesareo.francesco@gmail.com>
2022-03-04 16:14:25 +0100

    import md files using babel raw

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactMarkdown from "react-markdown";
-import content from "../assets/resources/landing-page.md";
+import raw from "raw.macro";
 
-const LandingPage = () => (
+const LandingPage = () => {
+    const content = raw("../assets/resources/landing-page.md");
+    return (
         <div className="container-fluid mt-3">
             <ReactMarkdown>{content}</ReactMarkdown>
         </div>
     );
+};
 
 export default LandingPage;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import {useIsAuthenticated} from "@azure/msal-react";
 import ReactMarkdown from "react-markdown";
+import raw from "raw.macro";
 import Topbar from "../components/Topbar";
-import content from "../assets/resources/login.md";
 
 const Login = () => {
     const isAuthenticated = useIsAuthenticated();
-
+    const content = raw("../assets/resources/login.md");
     return (
         <>
             <Topbar isAuthenticated={isAuthenticated}/>
