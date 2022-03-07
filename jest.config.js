c5250505b58eb8dc84505f36faac405e28fec039
Francesco Cesareo <cesareo.francesco@gmail.com>
2022-03-07 16:07:31 +0100

    fix unit test and lint

--- jest.config.js
+++ jest.config.js
@@ -89,7 +89,8 @@ module.exports = {
   // moduleNameMapper: {},
   "moduleNameMapper": {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
-    "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js"
+    "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js",
+    "react-markdown": "<rootDir>/__mocks__/react-markdown.tsx",
   },
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
